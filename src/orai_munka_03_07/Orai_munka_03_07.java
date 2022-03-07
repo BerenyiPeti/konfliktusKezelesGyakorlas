20dc7a8d9b0c9adf2db63d9481a54fbd584fb7ac
BerenyiPeti <berenyipeti01@gmail.com>
2022-03-07 15:06:59 +0100

    kezdes

---
+++ src/orai_munka_03_07/Orai_munka_03_07.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orai_munka_03_07;
+
+/**
+ *
+ * @author hallgato
+ */
+public class Orai_munka_03_07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nev1, nev2;
+    }
+    
+}
